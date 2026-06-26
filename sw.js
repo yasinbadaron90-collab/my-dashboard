3031951434de2034cf67182b96a24615139963db
yasinbadaron90-collab <yasin.badaron90@gmail.com>
2026-06-26 04:13:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
 // To force every client to upgrade: bump CACHE_VERSION below.
 // ════════════════════════════════════════════════════════════════════
 
-const CACHE_VERSION = 'v142o';
+const CACHE_VERSION = 'v142p';
 const SHELL_CACHE   = 'my-dashboard-shell-' + CACHE_VERSION;
 const CDN_CACHE     = 'my-dashboard-cdn-'   + CACHE_VERSION;
 
